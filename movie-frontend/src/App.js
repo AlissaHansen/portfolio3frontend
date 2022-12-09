84ea98a740a6608ac58ef7f2fbef05f51c40ae47
xframee <alexander@kmax.dk>
2022-12-09 14:31:37 +0100

    added new stylesheet with new bg color

--- movie-frontend/src/App.js
+++ movie-frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 import NavbarComponent from './Components/navbarComponent';
+import {useEffect} from 'react';
+import React from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document.body.classList.add('main-body');
+  });
+
   return (
-    <NavbarComponent/>
+      <NavbarComponent/>
   );
 }
 
