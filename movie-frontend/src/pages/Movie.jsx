bfc04464324db7b261d824a51239d897945ab3f7
Alissa Hansen <79077747+AlissaHansen@users.noreply.github.com>
2022-12-19 22:13:16 +0100

    added movie site

--- movie-frontend/src/pages/Movie.jsx
+++ movie-frontend/src/pages/Movie.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import MovieComponent from "../Components/MovieComponent";
 
 function Movie() {
-const location = useLocation();
+    const location = useLocation();
 
-const {endYear, isAdult, originalTitle, poster, primaryTitle, runTime, startYear, type, url} = location.state;
-
-console.log(location.state.primaryTitle);
+    const { originalTitle, poster, primaryTitle, runTime, startYear, type} = location.state;
 
     return (
-        <h1>{primaryTitle} og {runTime}</h1>
+        <>
+            <div className="movie-container">
+                <div className="movie-wrapper">
+                    <h1 className="movie-header">"{primaryTitle}"</h1>
+                    <img src={poster} alt={`Poster for ${primaryTitle}`} />
+                </div>
+                <div className="info-box">
+                    <ul>
+                        <li>Original Title: {originalTitle}</li>
+                        <li>Released in: {startYear}</li>
+                        <li>Runtime: {runTime} minutes</li>
+                        <li>Type: {type}</li>
+                    </ul>
+                </div>
+            </div>
+            <h2 className="Home-movie-header">Other movies you might wanna check out</h2>
+      <MovieComponent pagesize={20} page={12} />
+        </>
     );
 };
 
